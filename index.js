13563f0efff088581bb92f5b499b0a26682bdeaa
TravisWongX <travis.x.wong@gmail.com>
2021-07-21 11:45:04 +0800

    add cors

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { response, request } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
     return [
